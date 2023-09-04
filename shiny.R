85731b19f0213240ce495566ff9ad476630c05e6
Giuseppe Cinque <gfcinque@inwind.it>
2023-09-04 12:20:34 +0200

    aggiunta logo sapienza

--- shiny.R
+++ shiny.R
@@ -3,24 +3,175 @@ library(bslib)
 library(RColorBrewer)
 require(pals)
 
-# Define UI for app that draws a histogram ----
+my_autocomplete_list <- c("Benevento","Salerno","Bari","Taranto","Lecce"
+                          ,"Basilicata","Potenza","Matera","Caserta","Napoli"
+                          ,"Avellino","Foggia","Brindisi"
+                          ,"Calabria"
+                          ,"Cosenza"
+                          ,"Crotone"
+                          ,"Catanzaro"
+                          ,"Vibo Valentia"
+                          ,"Reggio di Calabria"
+                          ,"Isole"
+                          ,"Sicilia"
+                          ,"Trapani"
+                          ,"Palermo"
+                          ,"Messina"
+                          ,"Agrigento"
+                          ,"Caltanissetta"
+                          ,"Enna"
+                          ,"Catania"
+                          ,"Ragusa"
+                          ,"Siracusa"
+                          ,"Sardegna"
+                          ,"Sassari"
+                          ,"Nuoro"
+                          ,"Cagliari"
+                          ,"Oristano"
+                          ,"Monza e della Brianza"
+                          ,"Fermo"
+                          ,"Barletta-Andria-Trani"
+                          ,"Imperia"
+                          ,"La Spezia"
+                          ,"Como"
+                          ,"Milano"
+                          ,"Emilia-Romagna"
+                          ,"Reggio nell'Emilia"
+,"Verbano-Cusio-Ossola"
+,"Asti"
+,"Lecco"
+,"Bergamo"
+,"Lodi"
+,"Aosta"
+,"Savona"
+,"Vicenza"
+,"Friuli-Venezia Giulia"
+,"Gorizia"
+,"Piemonte"
+,"Nord-ovest"
+,"Vercelli"
+,"Sondrio"
+,"Brescia"
+,"Cremona"
+,"Trentino Alto Adige / Südtirol"
+,"Torino"
+,"Novara"
+,"Bolzano"
+,"Veneto"
+,"Belluno"
+,"Pavia"
+,"Mantova"
+,"Provincia Autonoma Bolzano"
+,"Trento"
+,"Biella"
+,"Cuneo"
+,"Alessandria"
+,"Liguria"
+,"Genova"
+,"Varese"
+,"Padova"
+,"Pordenone"
+,"Trieste"
+,"Parma"
+,"Provincia Autonoma Trento"
+,"Verona"
+,"Treviso"
+,"Rovigo"
+,"Venezia"
+,"Udine"
+,"Piacenza"
+,"Modena"
+,"Bologna"
+,"Ferrara"
+,"Ravenna"
+,"Forli'-Cesena"
+                          ,"Rimini"
+                          ,"Toscana"
+                          ,"Massa Carrara"
+                          ,"Lucca"
+                          ,"Pistoia"
+                          ,"Firenze"
+                          ,"Prato"
+                          ,"Livorno"
+                          ,"Pisa"
+                          ,"Arezzo"
+                          ,"Siena"
+                          ,"Grosseto"
+                          ,"Umbria"
+                          ,"Perugia"
+                          ,"Terni"                          ,"Marche"
+                          ,"Pesaro e Urbino"
+                          ,"Ancona"
+                          ,"Macerata"
+                          ,"Ascoli Piceno"
+                          ,"Lazio"
+                          ,"Viterbo"
+                          ,"Rieti"
+                          ,"Roma"
+                          ,"Latina"
+                          ,"Frosinone"
+                          ,"Abruzzo"
+                          ,"Pescara"
+                          ,"Chieti"
+                          ,"Molise"
+                          ,"Campobasso"
+                          ,"L'Aquila"
+,"Teramo"
+,"Isernia"
+,"Sud Sardegna")
+
 ui <- bootstrapPage(
   theme = bs_theme(version = 5, bootswatch = "minty"),
-  div(class="container-fluid",
-      div(class="text-center text-lg-start",
-        h3("Percentuale di stranieri residenti sulla popolazione totale (ISTAT, 2009 - 2019)", class="d-none d-lg-block"),
-        h3("Percentuale di stranieri (ISTAT)", class="d-block d-lg-none")
+  div(class = "container-fluid",
+      div(class = "jumbotron text-center bg-primary text-white",
+          h1("Analisi della Percentuale di Stranieri in Italia", class = "display-4"),
+          p("Esplora l'evoluzione del fenomeno migratorio dal 2002 al 2019", class = "lead")
       ),
-      div(class="row",
-          div(class="col-lg-2",
-              div(class="well",
-                  numericInput(inputId = "year", label = "Anno:", min = 2009, max = 2019, value = 2009)
+      div(class = "row justify-content-center mt-4",
+          div(class = "col-lg-4",
+              div(class = "card border-info",
+                  div(class = "card-body",
+                      h4("Seleziona un Anno", class = "card-title text-info"),
+                      sliderInput(inputId = "year", sep ="",label = NULL, min = 2002, max = 2019, value = 2002)
+                  )
+              ),
+              div(class = "card border-info",
+                  div(class = "card-body",
+                      h4("Inserisci una Provincia", class = "card-title text-info"),
+                      selectizeInput(
+                        inputId = 'search',
+                        label = 'Search',
+                        choices = my_autocomplete_list,
+                        selected = NULL,
+                        multiple = FALSE, # allow for multiple inputs
+                        options = list(create = FALSE) # if TRUE, allows newly created inputs
+                      ),
+                      
+                      textOutput("stranieri"),
+                      textOutput("totale"),
+                      textOutput("perc")
+                  )
+              ),
+              br(),
+              br(),
+              br(),
+              img(src="sapienza.png",
+                  height=120,
+                  width=247),
+              img(src="Logo.jpg",
+                  height=90,
+                  width=140
               )
           ),
-          div(class="col-lg-10", 
-              plotOutput(outputId = "distPlot", height="800")
+          div(class = "col-lg-8", 
+              div(class = "card border-success",
+                  div(class = "card-body",
+                      h4("Distribuzione della Percentuale di Stranieri a livello provinciale", class = "card-title text-success"),
+                      plotOutput(outputId = "distPlot", height = "700")
+                  )
+              )
           )
-    )
+      )
   )
 )
 
@@ -33,11 +184,11 @@ server <- function(input, output) {
   library(tidyr) #per eseguire la trasformazione long-wide, vedere sotto
   
   #importiamo il dataset con le coordinate geografiche per provincia
+  #coordinate=read_sf("/Users/giorgiatagliaferri/Desktop/esame Jona-Gubbiotti/ProvCM01012023_g/ProvCM01012023_g_WGS84.shp")
   coordinate=read_sf("ProvCM01012023_g_WGS84.shp")
   #selezioniamo solo le colonne che ci servono (nome e coordinate geografiche)
   coordinate=coordinate %>% select(c("DEN_UTS",
                                      "geometry"))
-  #View(coordinate)
   
   #importiamo il dataset con la popolazione residente per ogni provincia e per ogni anno dal 2002 al 2019
   #il file contiene la popolazione residente totale, quella degli stranieri e quella degli italiani
@@ -48,7 +199,6 @@ server <- function(input, output) {
                                         "TIME",
                                         "Value",
                                         "Cittadinanza"))
-  #View(popolazione)
   
   #il formato di questa tabella non è ottimale, vorrei avere per ogni provincia
   #e per ogni anno tre colonne. La prima con il numero di italiani, la seconda
@@ -57,7 +207,6 @@ server <- function(input, output) {
   popolazione_wide=popolazione %>% pivot_wider(names_from=Cittadinanza, values_from=Value)
   #rinomino alcune colonne
   names(popolazione_wide)=c("Territorio","Anno","italiano","straniero","totale")
-  #View(popolazione_wide)
   
   #creo la colonna che contiene il rapporto tra il numero di stranieri e il totale,
   #per ogni provincia e per ogni anno
@@ -66,38 +215,40 @@ server <- function(input, output) {
   #metto insieme la tabella con le coordinate geografiche e quella con i dati sulla
   #popolazione
   join=merge(y=popolazione_wide, x=coordinate, by.y="Territorio", by.x="DEN_UTS", all=F)
-  #View(join)
   
   #creo le classi di percentuale di stranieri, per poter visualizzare diversi colori
   #nei grafici
-  join$perc_class=cut(join$perc, c(0, 3, 5, 8, 10, 12, 15, 17.9))
+  join$perc_class=cut(join$perc, c(0, 3, 6, 9, 12, 15, 17.9))
   
-  #tramite un ciclo, faccio tanti grafici quanti sono gli anni
-  #for(t in seq(2002,2019)){
   output$distPlot <- renderPlot({
     ggplot(join %>% filter(Anno==input$year)) + #filtro per l'anno t
-    geom_sf(aes(fill = perc_class)) + #coloro per la classe
-    theme_bw()+ #solo un'impostazione grafica
-    #labs(title=t)+ #mette l'anno come titolo del grafico
-    labs(fill = "Percentuale di\nstranieri residenti\nsulla popolazione\ntotale (ISTAT)", ) +
-    scale_fill_brewer()+
-    theme(legend.justification = "center",
-            legend.direction = "horizontal", legend.position = "bottom")
-    #scale_fill_manual(values=c(pal.bands(coolwarm), brewer.pal(10,”Purples“)[5], "yellow","yellow3", "orange","orange3","red","red3")) #scelgo i colori delle classi
+      geom_sf(aes(fill = perc_class)) + #coloro per la classe
+      theme_bw()+ #solo un'impostazione grafica
+      labs(fill = "Percentuale di\nstranieri residenti\nsulla popolazione\ntotale (ISTAT)", ) +
+      theme(legend.justification = "center",
+            legend.direction = "horizontal", legend.position = "bottom")+
+      ggtitle(label=paste("Anno",input$year))+
+      scale_fill_manual(labels=c("x ≤ 3%", "3% < x ≤ 6%", "6% < x ≤ 9%", "9% < x ≤ 12%", "12% < x ≤ 15%", "x > 15%"), values = brewer.pal(6, "Blues")) #scelgo i colori delle classi
   })
   
+  output$stranieri <- renderText({
+    paste0("Residenti stranieri: ",
+           format(as.numeric(as.data.frame(join %>% filter(Anno==input$year, DEN_UTS==input$provincia)) %>% select(straniero)),
+                  big.mark=",")
+    )
+  })
+  output$totale <- renderText({
+    paste0("Popolazione residente totale: ",
+           format(as.numeric(as.data.frame(join %>% filter(Anno==input$year, DEN_UTS==input$provincia)) %>% select(totale)),
+                  big.mark=",")
+    )
+  })
+  output$perc <- renderText({
+    paste0("Percentuale di stranieri residenti: ",
+           round(as.numeric(as.data.frame(join %>% filter(Anno==input$year, DEN_UTS==input$provincia)) %>% select(perc)),digits=1),
+           "%")
+  })
   
-  
-  #print(plot) #printo il grafico
-  #}
-  # Histogram of the Old Faithful Geyser Data ----
-  # with requested number of bins
-  # This expression that generates a histogram is wrapped in a call
-  # to renderPlot to indicate that:
-  #
-  # 1. It is "reactive" and therefore should be automatically
-  #    re-executed when inputs (input$bins) change
-  # 2. Its output type is a plot
 }
 
 # Create Shiny app ----
